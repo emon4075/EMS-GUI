3ad2145fd6e73239c1291bab72257a49ee6c07d2
emon4075 <mdabdullahemon4075@gmail.com>
2024-08-17 23:24:40 +0600

    Designed Form

--- src/Project/My_Frame.java
+++ src/Project/My_Frame.java
@@ -4,17 +4,21 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 
 public class My_Frame extends JFrame implements ActionListener {
     ImageIcon frameIcon = new ImageIcon("E://Swing Project//assets//goat.png");
-    JPanel mainPanel;
-    JButton addButton, deleteButton, showButton, exitButton;
+    JPanel mainPanel, addUserPanel;
+    JButton addButton, deleteButton, showButton, exitButton, submitButton;
+    @SuppressWarnings("rawtypes")
+    JComboBox designationComboBox;
 
     My_Frame() {
         mainPanelFun();
@@ -37,15 +41,12 @@ public class My_Frame extends JFrame implements ActionListener {
         My_Labels myLabels = new My_Labels();
 
         /* Logo Label Start */
-
         JLabel logoLabel = myLabels.logoLabelFun();
         logoLabel.setBounds(100, 20, 400, 200);
         mainPanel.add(logoLabel);
-
         /* Logo Label End */
 
         /* Navigation Label & Adding Buttons With ActionListener Start */
-
         JLabel navigationLabel = myLabels.navigationLabelFun();
         navigationLabel.setBounds(220, 280, 150, 150);
 
@@ -54,24 +55,28 @@ public class My_Frame extends JFrame implements ActionListener {
         addButton.setBackground(new Color(200, 240, 35));
         addButton.setFont(new Font("Courier New", Font.PLAIN, 15));
         addButton.addActionListener(this);
+        addButton.setMnemonic(KeyEvent.VK_A);
 
         showButton = new JButton("Show User");
         showButton.setFocusable(false);
         showButton.setBackground(new Color(200, 240, 35));
         showButton.setFont(new Font("Courier New", Font.PLAIN, 15));
         showButton.addActionListener(this);
+        showButton.setMnemonic(KeyEvent.VK_S);
 
         deleteButton = new JButton("Delete User");
         deleteButton.setFocusable(false);
         deleteButton.setBackground(new Color(200, 240, 35));
         deleteButton.setFont(new Font("Courier New", Font.PLAIN, 15));
         deleteButton.addActionListener(this);
+        deleteButton.setMnemonic(KeyEvent.VK_D);
 
         exitButton = new JButton("Exit EMS");
         exitButton.setFocusable(false);
         exitButton.setBackground(new Color(200, 240, 35));
         exitButton.setFont(new Font("Courier New", Font.PLAIN, 15));
         exitButton.addActionListener(this);
+        exitButton.setMnemonic(KeyEvent.VK_X);
 
         navigationLabel.add(addButton);
         navigationLabel.add(showButton);
@@ -79,13 +84,55 @@ public class My_Frame extends JFrame implements ActionListener {
         navigationLabel.add(exitButton);
 
         mainPanel.add(navigationLabel);
-
         /* Navigation Label & Adding Buttons With ActionListener End */
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public void addUserPanelFun() {
+        addUserPanel = new JPanel();
+        addUserPanel.setBackground(new Color(103, 65, 136));
+        addUserPanel.setBounds(0, 0, 600, 600);
+        addUserPanel.setLayout(null);
+
+        My_Labels myLabels = new My_Labels();
+
+        /* addUserPanel + userFormPanel */
+        JLabel userFormLabel = myLabels.userFormLabelFun();
+        userFormLabel.setBounds(0, 0, 600, 600);
+        /* Baki Gulao Add Korte Hobe */
+
+        /* Designation */
+        String[] designations = { "Intern", "Lead Developer", "DevOps", "QA Engineer", "Team Lead" };
+        designationComboBox = new JComboBox(designations);
+        designationComboBox.setBounds(200, 250, 200, 30);
+        designationComboBox.setFont(new Font("Trebuchet MS", Font.PLAIN, 20));
+        designationComboBox.addActionListener(this);
+
+        /* Submit Button */
+        submitButton = new JButton("Submit");
+        submitButton.setFocusable(false);
+        submitButton.setBackground(new Color(200, 240, 35));
+        submitButton.setFont(new Font("Trebuchet MS", Font.PLAIN, 20));
+        submitButton.setBounds(250, 350, 100, 30);
+        submitButton.addActionListener(this);
+
+        addUserPanel.add(userFormLabel);
+        addUserPanel.add(designationComboBox);
+        addUserPanel.add(submitButton);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == exitButton) {
+        if (e.getSource() == addButton) {
+            this.remove(mainPanel);
+            addUserPanelFun();
+            this.add(addUserPanel);
+            this.revalidate();
+            this.repaint();
+        }
+        if (e.getSource() == submitButton) {
+            System.out.println("HAHA");
+        } else if (e.getSource() == exitButton) {
             System.exit(0);
         }
     }
--- src/Project/My_Labels.java
+++ src/Project/My_Labels.java
@@ -3,11 +3,14 @@ package Project;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 
 public class My_Labels {
     public JLabel logoLabelFun() {
+        /* Logo label implementation */
+
         ImageIcon logoIcon = new ImageIcon("E://Swing Project//assets//Main_Logo_Resized.png");
         JLabel logoLabel = new JLabel();
         logoLabel.setText("Employee Manager");
@@ -23,8 +26,57 @@ public class My_Labels {
     }
 
     public JLabel navigationLabelFun() {
+        /* Navigation label implementation */
+
         JLabel navigationLabel = new JLabel();
         navigationLabel.setLayout(new GridLayout(4, 1, 10, 10));
         return navigationLabel;
+    }
+
+    public JLabel userFormLabelFun() {
+        JLabel userFormLabel = new JLabel();
+        userFormLabel.setLayout(null);
+
+        /* Form Title */
+        JLabel formTitle = new JLabel("Add New Employee");
+        formTitle.setForeground(Color.WHITE);
+        formTitle.setFont(new Font("Trebuchet MS", Font.PLAIN, 30));
+        formTitle.setBounds(130, 80, 350, 30);
+        userFormLabel.add(formTitle);
+
+        /* User Name Label */
+        JLabel nameLabel = new JLabel("Name:");
+        nameLabel.setForeground(Color.WHITE);
+        nameLabel.setFont(new Font("Trebuchet MS", Font.PLAIN, 20));
+        nameLabel.setBounds(100, 150, 100, 30);
+        userFormLabel.add(nameLabel);
+
+        /* User Name Text Field */
+        JTextField nameField = new JTextField();
+        nameField.setFont(new Font("Trebuchet MS", Font.PLAIN, 20));
+        nameField.setBounds(200, 150, 200, 30);
+        userFormLabel.add(nameField);
+
+        /* User ID Label */
+        JLabel idLabel = new JLabel("User ID:");
+        idLabel.setForeground(Color.WHITE);
+        idLabel.setFont(new Font("Trebuchet MS", Font.PLAIN, 20));
+        idLabel.setBounds(100, 200, 100, 30);
+        userFormLabel.add(idLabel);
+
+        /* User ID Text Field */
+        JTextField idField = new JTextField();
+        idField.setFont(new Font("Trebuchet MS", Font.PLAIN, 20));
+        idField.setBounds(200, 200, 200, 30);
+        userFormLabel.add(idField);
+
+        /* User Designation */
+        JLabel designationLabel = new JLabel("Postion:");
+        designationLabel.setForeground(Color.WHITE);
+        designationLabel.setFont(new Font("Trebuchet MS", Font.PLAIN, 20));
+        designationLabel.setBounds(100, 250, 100, 30);
+        userFormLabel.add(designationLabel);
+
+        return userFormLabel;
     }
 }
